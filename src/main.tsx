45e54c9636ba65388cc01a0ed362964ec7933f8d
mohammadjmweb <mohammadjmweb@gmail.com>
2026-08-03 12:55:53 +0330

    create project

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+
+import App from "./App";
+
+import { ThemeProvider } from "./components/ThemeContext";
+
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+
+    <BrowserRouter>
+
+      <ThemeProvider>
+
+        <App />
+
+      </ThemeProvider>
+
+    </BrowserRouter>
+
+  </StrictMode>
+);
